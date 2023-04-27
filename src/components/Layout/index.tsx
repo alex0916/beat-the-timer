af89bf2dd37e171adcda326faa0754ab775d714b
Alex Baltodano <abaltodanop09@gmail.com>
2023-04-27 22:26:59 +0100

    feat: wait for window height

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,6 +3,11 @@ import { useWindowSize } from '@src/hooks';
 
 export const Layout = ({ children }: PropsWithChildren) => {
 	const { height } = useWindowSize();
+
+	if (!height) {
+		return null;
+	}
+
 	return (
 		<div style={{ height }} className="grid align-content-center">
 			<div className="col-10 col-offset-1 lg:col-4 lg:col-offset-4">{children}</div>
